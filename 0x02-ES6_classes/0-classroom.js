5c044a3528087055a1e3fc9d537330e95d04dd47
pitzee <pitzee421@gmail.com>
2023-09-29 17:25:27 +0000

    Implement a class named ClassRoom

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,6 @@
-export default class ClassRoom{
-	constructor(maxStudentsSize){
+export default class ClassRoom {
+
+	constructor(maxStudentsSize) { 
 		this._maxStudentsSize = maxStudentsSize;
 		}
 }
