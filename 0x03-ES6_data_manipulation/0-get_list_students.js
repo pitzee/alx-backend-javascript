e3f995fa2fc8bb0ec6ee413a96bd43f7fbfb9661
pitzee <pitzee421@gmail.com>
2023-10-03 19:20:12 +0000

    0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,19 @@
-export default function getListStudents() {
-  const student = [
-    { id: 1, firstname: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstname: 'James', location: 'Columbia' },
-    { id: 5, firstname: 'Serena', location: 'San Francisco' },
-  ];
-  return student;
-}
+const getListStudents = () => [
+  {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  },
+  {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  },
+  {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  },
+];
+
+export default getListStudents;
