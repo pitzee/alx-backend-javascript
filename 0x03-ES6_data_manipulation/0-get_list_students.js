39fe9c45d76ed3fdf7d55516c12ec79b28a37de1
pitzee <pitzee421@gmail.com>
2023-10-03 17:58:30 +0000

    Basic list of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,8 @@
-export default function getListStudents(){
+export default function getListStudents() {
   const student = [
-    { id:1, firstname: 'Guillaume', location: 'San Francisco'},
-    {id:2, firstname: 'James', location: 'Columbia'},
-    {id:5, firstname: 'Serena', location: 'San Francisco'}
-];
-return student;
+    { id: 1, firstname: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstname: 'James', location: 'Columbia' },
+    { id: 5, firstname: 'Serena', location: 'San Francisco' },
+  ];
+  return student;
 }
